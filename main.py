37969b2d310a1b7cc19dc56590970b732d6c07ec
qs-ashish05 <aashishjha8080@gmail.com>
2022-07-10 20:44:35 +0530

    test

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class main(object):
 
         for img in images:
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            encode = face_recognition.face_encodings(img)   #[0]
+            encode = face_recognition.face_encodings(img)[0:]#[0]
             encodeList.append(encode)
         return encodeList
 
